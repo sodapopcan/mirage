0e88e48ac3b4540600aa50af5c1b22acf4f6f29f
Andrew Haust <andrew@hau.st>
2026-04-16 11:43:23 -0400

    Cleanup

--- lib/holo_test.ex
+++ lib/holo_test.ex
@@ -6,31 +6,47 @@ defmodule HoloTest do
   template into a fully-resolved DOM that tests can make assertions against.
   """
 
+  defmodule HoloTest.Session do
+    @moduledoc """
+    Represents a test session — the state of a page after a `HoloTest.visit/2`.
+    """
+
+    alias Hologram.Component
+
+    defstruct [:page, :ast]
+
+    @type t :: %__MODULE__{
+            page: Component.t(),
+            ast: any()
+          }
+  end
+
+  alias Hologram.Page
+  alias HoloTest.Session
   alias Hologram.Server
   alias HoloTest.DOM
-  alias HoloTest.Session
 
   @doc """
   Visits a Hologram page module and returns a `HoloTest.Session` containing
   the initialized page struct and the expanded, layout-wrapped DOM.
   """
-  @spec visit(module, %{atom => any}) :: Session.t()
+  @spec visit(Page.t(), %{atom() => any()}) :: Session.t()
   def visit(page_module, params \\ %{}) do
-    {page_struct, server} = DOM.init_component(page_module, params, %Server{})
+    {page, server} = DOM.init_component(page_module, params, %Server{})
 
-    vars = Map.merge(params, page_struct.state)
+    vars = Map.merge(params, page.state)
     page_dom = page_module.template().(vars)
 
     layout_props_dom =
       page_module.__layout_props__()
       |> Enum.into(%{cid: "layout"})
-      |> Map.merge(page_struct.state)
+      |> Map.merge(page.state)
       |> Enum.map(fn {name, value} -> {to_string(name), [expression: {value}]} end)
 
     root = {:component, page_module.__layout_module__(), layout_props_dom, page_dom}
-    env = %{context: page_struct.emitted_context, slots: []}
+    env = %{context: page.emitted_context, slots: []}
     ast = DOM.expand(root, env, server)
 
-    %Session{page: page_struct, ast: ast}
+    %Session{page: page, ast: ast}
   end
 end
