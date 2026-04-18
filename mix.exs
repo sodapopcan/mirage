10ac7e6abb7f5e1cfcd535e8d86f1a1d3058a0c6
Andrew Haust <andrew@hau.st>
2026-04-18 13:49:16 -0400

    Set version to 0.0.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Mirage.MixProject do
   def project do
     [
       app: :mirage,
-      version: "0.1.0",
+      version: "0.0.1",
       elixir: "~> 1.19",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
