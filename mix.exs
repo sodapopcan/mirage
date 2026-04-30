d0dedacd6e678e3b20b5063fcb9f016a6b01d22b
Andrew Haust <andrew@hau.st>
2026-04-30 12:52:26 -0400

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Mirage.MixProject do
   def project do
     [
       app: :mirage,
-      version: "0.5.0",
+      version: "0.6.0",
       elixir: "~> 1.19",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:leex] ++ Mix.compilers(),
